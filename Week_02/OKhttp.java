9b14a0ab7248e47a5d0ecc166a35cb6b339657bd
knightzhwb <71489060+knightzhwb@users.noreply.github.com>
2021-01-22 23:44:18 +0800

    周日

---
+++ Week_02/OKhttp.java
@@ -0,0 +1,33 @@
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @Title:
+ * @Description:
+ * @Author:Zhang wenbin
+ * @Date:2021/1/22
+ */
+public class OKhttp {
+    public static void main(String[] args) {
+        String url = "http://localhost:8801/";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                System.out.println("请求失败");
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                System.out.println("响应：" + Objects.requireNonNull(response.body()).string());
+            }
+        });
+    }
+}
